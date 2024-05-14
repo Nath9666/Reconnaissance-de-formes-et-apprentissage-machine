0f45e62f98ac054b4c238e782a7b84f1c9530c4e
Nathan <93925029+Nath9666@users.noreply.github.com>
2024-05-14 09:04:32 +0200

    ex2.2

--- TP/TP1/Ex2.py
+++ TP/TP1/Ex2.py
@@ -1,4 +1,4 @@
-import Ex1
+from Ex1 import *
 from sklearn.datasets import load_iris
 import pandas as pd
 
@@ -8,17 +8,31 @@ iris = load_iris()
 # Créer un DataFrame à partir des données iris
 iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 
-# Afficher les premières lignes du DataFrame
-print("First 5 rows of the DataFrame:")
-print(iris_df.head())
-print("-----------------------------")
+def explore_dataset (df: pd.DataFrame) -> None:
+    """
+    Explore the given dataset by displaying the first 5 rows, the information and the descriptive statistics of the DataFrame.
 
-# Afficher les informations sur le DataFrame
-print("Information about the DataFrame:")
-print(iris_df.info())
-print("-----------------------------")
+    Parameters:
+        df (pd.DataFrame): The DataFrame to be explored.
 
-# Afficher les statistiques descriptives du DataFrame
-print("Descriptive statistics of the DataFrame:")
-print(iris_df.describe())
-print("-----------------------------")
+    Returns:
+        None
+
+    """
+    print("First 5 rows of the DataFrame:")
+    print(df.head())
+    print("-----------------------------")
+
+    print("Information about the DataFrame:")
+    print(df.info())
+    print("-----------------------------")
+
+    print("Descriptive statistics of the DataFrame:")
+    print(df.describe())
+    print("-----------------------------")
+
+# Appliquer l'ACP avec 2 composantes principales
+transformed_data, explained_variance_ratio = compute_ACP(iris.data, n_components=2)
+
+# Vérifier la dimension des données après application de l'ACP
+print("Dimension des donnees apres application de l'ACP :", transformed_data.shape)
