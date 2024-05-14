663a4583db854a906953c1be205d0f00d63b2f3f
Nathan <93925029+Nath9666@users.noreply.github.com>
2024-05-14 10:34:51 +0200

    ex 3

--- TP/TP1/Ex3.py
+++ TP/TP1/Ex3.py
@@ -3,7 +3,7 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
 # Charger les données
-df = pd.read_csv('./leaf/leaf.csv')
+df = pd.read_csv('C:\\Users\\Nathan\\Documents\\Projet\\Reconnaissance de formes et apprentissage machine\\TP\\TP1\\leaf\\leaf.csv')
 
 # Appliquer l'ACP
 pca = PCA()
@@ -19,4 +19,28 @@ plt.xlabel('Nombre de composantes')
 plt.ylabel('Valeurs propres')
 plt.title('Décroissance des valeurs propres')
 plt.grid()
+plt.show()
+
+from mpl_toolkits.mplot3d import Axes3D
+
+# Appliquer l'ACP avec 3 composantes principales
+pca = PCA(n_components=3)
+pca_result = pca.fit_transform(df.iloc[:, :-1])
+
+# Créer un graphique 3D
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
+
+# Utiliser l'étiquette de classe pour donner une couleur à chaque classe
+scatter = ax.scatter(pca_result[:, 0], pca_result[:, 1], pca_result[:, 2], c=df.iloc[:, -1])
+
+# Ajouter une légende
+legend1 = ax.legend(*scatter.legend_elements(), title="Classes")
+ax.add_artist(legend1)
+
+# Ajouter des étiquettes d'axe
+ax.set_xlabel('PC1')
+ax.set_ylabel('PC2')
+ax.set_zlabel('PC3')
+
 plt.show()
