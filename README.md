b6feda83909d099b4dfd091c67bbf3aca8171650
Nathan <93925029+Nath9666@users.noreply.github.com>
2024-05-14 11:42:52 +0200

    add readme and replot

---
+++ README.md
@@ -0,0 +1,28 @@
+# Reconnaissance de formes et apprentissage machine
+
+Dans ce depot, vous trouverez les codes sources des TP de Reconnaissance de formes et apprentissage machine.
+
+Ansi que les CMs et TDs.
+
+## CM
+[Intoduction à la reconnaissance de formes](.\CM\ST2IAI-CM1.pdf)
+[ACP](.\CM\ST2IAI-PCA.pdf)
+
+## TP
+
+### TP1
+
+Dans celui-ci on voit les base de l'acp et de l'analyse discriminante.
+Plusieur ecercices nous son donnée pour nous aider à comprendre les notions.
+Chaque exercice est accompagné d'un code source en python ansi que d'un compte rendu en ipynb.
+
+[Exercice 1 Notebook](./TP/TP1/Ex1.ipynb)
+[Exercice 1 Code](./TP/TP1/Ex1.py)
+[Exercice 2 Notebook](./TP/TP1/Ex2.ipynb)
+[Exercice 2 Code](./TP/TP1/Ex2.py)
+[Exercice 3 Notebook](./TP/TP1/Ex3.ipynb)
+[Exercice 3 Code](./TP/TP1/Ex3.py)
+[Exercice 4 Notebook](./TP/TP1/Ex4.ipynb)
+[Exercice 4 Code](./TP/TP1/Ex4.py)
+[Partie2 Notebook](./TP/TP1/Partie2.ipynb)
+[Partie2 Code](./TP/TP1/Partie2.py)
--- TP/TP1/Ex3.py
+++ TP/TP1/Ex3.py
@@ -1,46 +1,118 @@
-import pandas as pd
-from sklearn.decomposition import PCA
+import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import datasets
+import sklearn.decomposition
+import sklearn.preprocessing
 
-# Charger les données
-df = pd.read_csv('C:\\Users\\Nathan\\Documents\\Projet\\Reconnaissance de formes et apprentissage machine\\TP\\TP1\\leaf\\leaf.csv')
+def compute_ACP(data, n_components = None):
+    return sklearn.decomposition.PCA(n_components = n_components).fit_transform(data)
 
-# Appliquer l'ACP
-pca = PCA()
-pca.fit(df)
+def variance_percentage(data, n_components = None):
+    data = sklearn.decomposition.PCA(n_components = n_components).fit(data)
+    return data.explained_variance_ratio_
 
-# Obtenir les valeurs propres
-eigenvalues = pca.explained_variance_
 
-# Créer un graphique pour la décroissance des valeurs propres
-plt.figure(figsize=(8, 6))
-plt.plot(range(1, len(eigenvalues) + 1), eigenvalues)
-plt.xlabel('Nombre de composantes')
-plt.ylabel('Valeurs propres')
-plt.title('Décroissance des valeurs propres')
-plt.grid()
-plt.show()
+    
 
-from mpl_toolkits.mplot3d import Axes3D
 
-# Appliquer l'ACP avec 3 composantes principales
-pca = PCA(n_components=3)
-pca_result = pca.fit_transform(df.iloc[:, :-1])
+## Exercice 2
 
-# Créer un graphique 3D
-fig = plt.figure(figsize=(8, 6))
-ax = fig.add_subplot(111, projection='3d')
+def acp_with_components(data, n_components):
+    pca = sklearn.decomposition.PCA(n_components = n_components)
+    pca.fit_transform(data)
+    transformed_data = compute_ACP(data, n_components)
+    
+    print("PCA with ", n_components, " principal components:")
+    print(transformed_data)
 
-# Utiliser l'étiquette de classe pour donner une couleur à chaque classe
-scatter = ax.scatter(pca_result[:, 0], pca_result[:, 1], pca_result[:, 2], c=df.iloc[:, -1])
+    print("Principal Components:")
+    print(pca.components_)
 
-# Ajouter une légende
-legend1 = ax.legend(*scatter.legend_elements(), title="Classes")
-ax.add_artist(legend1)
+    return transformed_data
 
-# Ajouter des étiquettes d'axe
-ax.set_xlabel('PC1')
-ax.set_ylabel('PC2')
-ax.set_zlabel('PC3')
 
-plt.show()
+
+def display_PCA(data, dimensions, color = None): # Display PCA with 2 or 3 principal components
+    transformed_data = compute_ACP(data, dimensions)
+
+    if dimensions == 2:
+        plt.scatter(transformed_data[:, 0], transformed_data[:, 1], c = color)
+        plt.xlabel('Component 1')
+        plt.ylabel('Component 2')
+    elif dimensions == 3:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(transformed_data[:, 0], transformed_data[:, 1], transformed_data[:, 2], c = color)
+    plt.title('PCA of Iris dataset')
+    plt.show()
+
+
+
+def dimensions_needed(data, threshold):
+    pca = sklearn.decomposition.PCA(n_components = len(data[0]))
+    pca.fit_transform(data)
+    variance_cumulative = np.cumsum(pca.explained_variance_ratio_)
+    dimensions_needed = np.argmax(variance_cumulative >= threshold) + 1
+    print("Number of dimensions needed to preserve 95% of the variance:", dimensions_needed)
+
+
+
+def dimensions_needed2(data, threshold):
+    n_components = 1
+    pca = sklearn.decomposition.PCA(n_components = n_components)
+    pca.fit_transform(data)
+    while pca.explained_variance_ratio_.sum() < threshold:
+        n_components += 1
+        pca = sklearn.decomposition.PCA(n_components = n_components)
+        pca.fit_transform(data)
+    print("Number of dimensions needed to preserve 95% of the variance:", n_components)
+
+
+
+def display_explained_variance(data):
+    pca = sklearn.decomposition.PCA(n_components = len(data[0]))
+    pca.fit_transform(data)
+    cumulative_explained_variance = np.cumsum(pca.explained_variance_ratio_)
+    for i in range(len(cumulative_explained_variance)):
+        print("Explained variance of component", i + 1, ":", cumulative_explained_variance[i])
+
+
+
+def display_cumulative_explained_variance(data):
+    pca = sklearn.decomposition.PCA(n_components = len(data[0]))
+    pca.fit_transform(data)
+    cumulative_explained_variance = np.cumsum(pca.explained_variance_ratio_)
+    plt.figure(figsize=(8, 6))
+    plt.plot(range(1, len(cumulative_explained_variance) + 1), cumulative_explained_variance)
+    plt.xlabel('Number of dimensions')
+    plt.ylabel('Cumulative explained variance')
+    plt.title('Contribution of variance according to the number of dimensions')
+    plt.grid()
+    plt.show()
+
+
+## Exercice 3
+
+def display_eigenvalues_decrease(data):
+    pca = sklearn.decomposition.PCA(n_components = len(data[0]))
+    pca.fit_transform(data)
+    eigenvalues = pca.explained_variance_
+    plt.figure(figsize = (8, 6))
+    plt.plot(range(1, len(eigenvalues) + 1), eigenvalues)
+    plt.xlabel('Number of dimensions')
+    plt.ylabel('Eigenvalues')
+    plt.title('Eigenvalues according to the number of dimensions')
+    plt.grid()
+    plt.show()
+
+
+
+def mainEx3():
+    data = np.genfromtxt('C:\\Users\\Nathan\\Documents\\Projet\\Reconnaissance de formes et apprentissage machine\\TP\\TP1\\leaf\\leaf.csv', delimiter=',')
+    display_eigenvalues_decrease(data[:, 1:])
+    display_PCA(data[:, 1:], 3, data[:, 0])
+
+if __name__ == '__main__':
+    mainEx3()
+
