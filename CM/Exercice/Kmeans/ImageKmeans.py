3cc8c81cadb612e0397af544cf10be2bcfd16865
Nath9666 <nathan.morel@efrei.net>
2024-06-02 14:51:37 +0200

    add telechargement

--- CM/Exercice/Kmeans/ImageKmeans.py
+++ CM/Exercice/Kmeans/ImageKmeans.py
@@ -2,17 +2,21 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import os
 
-def kmeans_segmentation(path, k_values, color=False):
+def kmeans_segmentation(path, k_values, color=False, output_dir='./output_images'):
     # Lire l'image
     if color:
         img = Image.open(path)
         img_np = np.array(img)
         pixels = img_np.reshape(-1, 1)
+        type='RGB'
     else:
         img = Image.open(path).convert('L')
         img_np = np.array(img)
         pixels = img_np.reshape(-1, 1)
+        type='grayscale'
+    name = os.path.basename(path)
 
     # Afficher l'histogramme
     plt.hist(img_np.ravel(), bins=256, color='black')
@@ -35,10 +39,15 @@ def kmeans_segmentation(path, k_values, color=False):
         # Afficher l'image segmentée
         plt.figure()
         plt.imshow(segmented_img, cmap='gray' if not color else None)
+        
+        # Enregistrer l'image segmentée
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        plt.imsave(os.path.join(output_dir, f'{name}_segmented_k{k}_{type}.jpeg'), segmented_img, cmap='gray' if not color else None)
     plt.show()
 
 # Utilisation de la fonction
-PATH = 'CM\\Exercice\\Kmeans\\arbre.jpg'
+PATH = 'C:\\Users\\Nathan\\Documents\\Projet\\Reconnaissance-de-formes-et-apprentissage-machine\\CM\\Exercice\\Kmeans\\dog.jpeg'
 k_values = [2, 3, 4, 5, 10]
 
 kmeans_segmentation(PATH, k_values, color=False)
